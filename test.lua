77fdf3e1caa995c20e5ad36979d320f87b2e39a9
Kristofer Karlsson <krka@spotify.com>
2013-10-14 11:55:53 +0200

    Initial git import

---
+++ test.lua
@@ -0,0 +1,18 @@
+--[[ long comment ]]
+
+--[==[ Another long comment ]==]
+
+if a then 
+b = c
+else
+d = e
+end
+
+--[==[ Another long [[ ]]
+
+
+[===[ Hello ]===] ]=]
+ comment ]==]
+
+if x == [=[this is a string ]=] then
+end
