b5ce6c46d9d866a9e3528a00f00c3800b4f2884c
Kristofer Karlsson <kristofer.karlsson@gmail.com>
2013-10-17 22:06:08 +0200

    Move out the test code

--- test.lua
+++ test.lua
@@ -1,18 +1,53 @@
---[[ long comment ]]
+require 'indent'
 
---[==[ Another long comment ]==]
-
-if a then 
-b = c
-else
-d = e
+function readAll(file)
+    local f = io.open(file, "rb")
+    if not f then return "" end
+    local content = f:read("*all")
+    f:close()
+    return content
 end
 
---[==[ Another long [[ ]]
-
-
-[===[ Hello ]===] ]=]
- comment ]==]
-
-if x == [=[this is a string ]=] then
+-- currently unused, just saved for future refactoring
+function testTokenizer()
+   local str = ""
+   for line in io.lines("indent.lua") do
+      str = str .. line .. "\n"
+   end
+   
+   local pos = 1
+   
+   while true do
+      local tokenType, nextPos = nextToken(str, pos)
+      
+      if not tokenType then
+         break
+      end
+      
+      if true or tokenType ~= tokens.TOKEN_WHITESPACE and tokenType ~= tokens.TOKEN_LINEBREAK then
+         print(string.format("Found token %d (%d-%d): (%s)", tokenType, pos, nextPos - 1, stringsub(str, pos, nextPos - 1)))
+      end
+      
+      if tokenType == tokens.TOKEN_UNKNOWN then
+         print("unknown token!")
+         break
+      end         
+      
+      pos = nextPos
+   end
 end
+
+
+function testIndenter(i)
+   local lib = IndentationLib
+   local input = readAll("test_input.txt")
+   local output = readAll("test_output.txt")
+   
+   local colorTable = lib.defaultColorTable
+   local actual = lib.indentCode(input, 4, colorTable, i)
+   assert(output == actual, "Failure, got:\n" .. actual)
+end
+   
+
+testIndenter()
+
